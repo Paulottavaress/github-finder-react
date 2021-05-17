be0f44f91c1db4180baa751b25538f97121ffe11
Paulo Henrique Tavares de Andrade <ph.and@Hotmail.com>
2021-05-17 20:08:34 -0300

    Users created

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
 import Navbar from './components/layout/Navbar';
-import UserItem from './components/users/UserItem';
+import Users from './components/users/Users';
 import './App.css';
 
 class App extends Component{
   render(){
-    const numbers = [1,2,3,4];
-
     return (
       <div className='App'>
         <Navbar icon={"fab fa-github"} title={"Github Finder"} />
-        <UserItem />
+        <div className="container">
+          <Users />
+        </div>
       </div>
     );
   }
--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
 
-export class Navbar extends Component {
+class Navbar extends Component {
     static defaultProps = {
         title: 'Github Finder',
         icon: 'fab fa-github'
