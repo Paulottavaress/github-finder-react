46727f0a1c4a3e57d651b5641f307df047e8af37
Paulo Henrique Tavares de Andrade <ph.and@Hotmail.com>
2021-05-23 19:58:44 -0300

    update on the search bar

--- src/App.js
+++ src/App.js
@@ -11,19 +11,24 @@ class App extends Component{
     loading: false
   }
 
+  // async componentDidMount() {
+  //   this.setState({ loading: true });
+  //   const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+  //   this.setState({ users: res.data, loading: false});
+  // }
 
-  async componentDidMount() {
-    this.setState({ loading: true });
+  // Search Github users
+  searchUsers = async text => {
     const res = await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
-    this.setState({ users: res.data, loading: false});
-  }
+    this.setState({ users: res.data.items, loading: false});
+  };
 
   render(){
     return (
       <div className='App'>
         <Navbar icon={"fab fa-github"} title={"Github Finder"} />
         <div className="container">
-          <Search />
+          <Search searchUsers={this.searchUsers}/>
           <Users loading={this.state.loading} users={this.state.users}/>
         </div>
       </div>
--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -7,15 +7,16 @@ export class Search extends Component {
 
     onSubmit = e => {
         e.preventDefault();
-        console.log(this.state.text);
-    }
+        this.props.searchUsers(this.state.text);
+        this.setState({ text: ''});
+    };
 
     onChange = (e) => this.setState ({ [e.target.name]: e.target.value});
 
     render() {
         return (
             <div>
-                <form onSubsmit={this.onSubmit} action="" className="form">
+                <form onSubmit={this.onSubmit} action="" className="form">
                     <input type="text" name="text" placeholder="Search Users..." value={this.state.text} onChange={this.onChange}/>
                     <input type="submit" value="Search" className="btn btn-dark btn-block"/>
                 </form>
