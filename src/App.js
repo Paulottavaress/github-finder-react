9a1cf928a6ba5011aa535a8a8f9689eab7c29152
Paulo Henrique Tavares de Andrade <ph.and@Hotmail.com>
2021-05-27 23:48:24 -0300

    clear button added

--- src/App.js
+++ src/App.js
@@ -20,13 +20,18 @@ class App extends Component{
     this.setState({ users: res.data.items, loading: false});
   };
 
+  // Clear users from state
+  clearUsers = () => this.setState({ users: [], loading: false });
+
   render(){
+    const { users, loading } = this.state;
+
     return (
       <div className='App'>
         <Navbar icon={"fab fa-github"} title={"Github Finder"} />
         <div className="container">
-          <Search searchUsers={this.searchUsers}/>
-          <Users loading={this.state.loading} users={this.state.users}/>
+          <Search searchUsers={this.searchUsers} clearUsers={this.clearUsers} showClear={users.length > 0 ? true: false}/>
+          <Users loading={loading} users={users}/>
         </div>
       </div>
     );
--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -7,7 +7,9 @@ export class Search extends Component {
     };
 
     static propTypes = {
-        searchUsers: PropTypes.func.isRequired
+        searchUsers: PropTypes.func.isRequired,
+        clearUsers: PropTypes.func.isRequired,
+        showClear: PropTypes.bool.isRequired
     };
 
     onSubmit = e => {
@@ -19,12 +21,17 @@ export class Search extends Component {
     onChange = (e) => this.setState ({ [e.target.name]: e.target.value});
 
     render() {
+        const { showClear, clearUsers } = this.props;
+        
         return (
             <div>
                 <form onSubmit={this.onSubmit} action="" className="form">
                     <input type="text" name="text" placeholder="Search Users..." value={this.state.text} onChange={this.onChange}/>
                     <input type="submit" value="Search" className="btn btn-dark btn-block"/>
                 </form>
+                {showClear && (
+                    <button className="btn btn-light btn-block" onClick={clearUsers}>Clear</button>
+                )} 
             </div>
         )
     }
