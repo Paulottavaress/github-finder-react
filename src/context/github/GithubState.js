cf8e0707c74384d2ddaa2993460ed4e414228217
Paulo Henrique Tavares de Andrade <ph.and@Hotmail.com>
2021-06-15 08:39:34 -0300

    Moved repos state to context

--- src/context/github/GithubState.js
+++ src/context/github/GithubState.js
@@ -45,6 +45,16 @@ const GithubState = props => {
       };
 
     // Get Repos
+    const getUserRepos = async (username) => {
+        setLoading();
+    
+        const res = await axios.get(`https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+    
+        dispatch({
+            type: GET_REPOS,
+            payload: res.data
+        });
+      };
 
     // Clear Users
     const clearUsers = () => dispatch({ type: CLEAR_USERS});
@@ -61,7 +71,8 @@ const GithubState = props => {
             loading: state.loading,
             searchUsers,
             clearUsers,
-            getUser
+            getUser,
+            getUserRepos
         }}
     >
         {props.children}
