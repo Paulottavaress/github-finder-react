b8aaf4f9fe04e9f71818356d079746eef39f8d72
Paulo Henrique Tavares de Andrade <ph.and@Hotmail.com>
2021-06-17 21:28:47 -0300

    home and not found created

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Alert from './components/layout/Alert';
 import Navbar from './components/layout/Navbar';
-import Users from './components/users/Users';
 import User from './components/users/User';
-import Search from './components/users/Search';
 import About from './components/pages/About';
-
+import Home from './components/pages/Home';
 import GithubState from './context/github/GithubState';
 import AlertState from './context/alert/AlertState';
+import NotFound from './components/pages/NotFound';
 
 import './App.css';
 
@@ -22,14 +21,10 @@ const App = () => {
             <div className="container">
               <Alert />
               <Switch>
-                <Route exact path='/' render={props => (
-                  <Fragment>
-                    <Search />
-                    <Users />
-                  </Fragment>
-                )}/>
+                <Route exact path='/' component={Home}/>
                 <Route exact path='/about' component={About} />
                 <Route exact path='/user/:login' component={User} />
+                <Route component={NotFound} />
               </Switch>
             </div>
           </div>
